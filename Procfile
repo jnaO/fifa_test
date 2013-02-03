1c2c75b1664e60dd1980a6623d9851a55926f8cd
Jan Lofberg <chenebu@gmail.com>
2013-02-03 22:40:33 +0100

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python fifa.py
--- fifa.py
+++ fifa.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -24,5 +25,6 @@ def history_cup(cup_name):
 #         show_the_login_form()
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    # app.debug = True
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
